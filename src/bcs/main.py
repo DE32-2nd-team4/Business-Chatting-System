ee42b6f1e7a6cab2eb44cb0cf9a53e3e715ac098
WhiteCapella <whitecapella.official@gmail.com>
2024-08-26 12:45:33 +0900

    local

--- src/bcs/main.py
+++ src/bcs/main.py
@@ -3,13 +3,23 @@ import time
 import json
 import threading
 import logging
+import os
 
 def send_message():
     global username
     global chatroom
-    log_file_path = f"/home/ubuntu/team4/{chatroom}/chat.log"  
+    
+    
+    log_file_path = f"~/atmp/chatdata/{chatroom}/chat.log"
+    log_file_path = os.path.expanduser(log_file_path)
     # 로그 파일 경로 (EC2 서버의 team4 폴더 아래 chatroom 폴더)
-    logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(message)s')
+    chatroom_dir = os.path.dirname(log_file_path)
+    if not os.path.exists(chatroom_dir):
+        os.makedirs(chatroom_dir)
+
+# chat.log 파일 터치 (존재하지 않을 경우 생성)
+    if not os.path.exists(log_file_path):
+        open(log_file_path, 'a').close()  # 파일 생성
     producer = KafkaProducer (
         bootstrap_servers=['ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092'],
         value_serializer=lambda x:json.dumps(x).encode('utf-8'),
@@ -17,13 +27,14 @@ def send_message():
 
 
     while(True):
-        message = input(">> ")  # 사용자 입력 받기
+        message = input(" ")  # 사용자 입력 받기
         if message == "exit":
             producer.close()
             break
         m_message = {'nickname': username, 'message': message, 'time':time.time()}
         producer.send(chatroom, value=m_message)
-        logging.info(json.dumps(m_message))
+        with open(log_file_path, 'a') as log_file:
+            log_file.write(json.dumps(m_message) + '\n')  # 줄바꿈 추가
         # server/team4/chatroom/chat.log
         # m_message를 chat.log에 삽입
         producer.flush()  # 메시지 전송 완료
