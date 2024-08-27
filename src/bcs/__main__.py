44e3504b76db2e6b57c146af1551965515ab2545
WhiteCapella <whitecapella.official@gmail.com>
2024-08-27 15:42:35 +0900

    Version 1.0 description

--- src/bcs/__main__.py
+++ src/bcs/__main__.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import subprocess
 
 def main():
     parser = argparse.ArgumentParser(description='BCS 도움말', add_help=False)
@@ -9,32 +10,38 @@ def main():
     parser.add_argument('-i', '--ipconfig', action='store_true', help='config/ip 파일 열기')
     args = parser.parse_args()
     if args.help:
-        print("Business Chatting System")  # 실제 도움 화면 내용으로 채워야 합니다.
-        print()
-        print("version 1.0")
-        print()
-        print()
-        print()
-        print()
-        print("Commands")
-        print("bcs, bcs -h, --help     : Show command help screen")
-        print("         -c, --chat     : Enter chatting system")
-        print("         -a, --audit    : Enter chat audit system")
-        print("         -i, --ipconfig : Open config ip file")
-        print()
-        print()
-        print()
-        print()
+        print("""
+        Business Chatting System
+        Version 1.0
 
-    elif args.c:
+        This repository is a package that combines business chat, chat auditing, and chatbot systems. 
+        It allows you to communicate securely with colleagues in other departments in security-sensitive work environments, 
+        The versatile chatbots included can boost productivity, 
+        and the system can quickly detect any potential threats or conspiracies against them. 
+        All of this is possible simply by utilizing this repository.
+        
+        
+        
+        Commands
+        bcs, bcs -h, --help     : Show command help screen
+                 -c, --chat     : Enter chatting system
+                 -a, --audit    : Enter chat audit system
+                 -i, --ipconfig : Open config ip file
+        
+        
+        
+        
+        """)
+    elif args.chat:
         # src/bcs/main.py 실행 로직 추가
         subprocess.run(["python", "src/bcs/main.py"])
-    elif args.a:
+    elif args.audit:
         # Zeppelin 실행 및 audit/data 폴더 열기 로직 추가
         print("Zeppelin 실행 및 audit/data 폴더 열기")
-    elif args.i:
+    elif args.ipconfig:
         # config/ip 파일 열기 로직 추가
         subprocess.run(["vim", "config/ip"])
+
 
 if __name__ == '__main__':
     main()
