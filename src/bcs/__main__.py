21169107850b69a696bfb1d87c4121ba28a877bf
WhiteCapella <whitecapella.official@gmail.com>
2024-08-28 16:28:25 +0900

    help program update

--- src/bcs/__main__.py
+++ src/bcs/__main__.py
@@ -6,7 +6,6 @@ def main():
     parser = argparse.ArgumentParser(description='BCS 도움말', add_help=False)
     parser.add_argument('-h', '--help', action='store_true', help='도움말 표시')  # -h와 --help 옵션을 한 번만 정의
     parser.add_argument('-c', '--chat', action='store_true', help='src/bcs/main.py 실행')
-    parser.add_argument('-a', '--audit', action='store_true', help='Zeppelin 실행 및 audit/data 폴더 열기')
     parser.add_argument('-i', '--ipconfig', action='store_true', help='config/ip 파일 열기')
     parser.add_argument('-b', '--bot', action='store_true', help='모든 봇 시작')
     args = parser.parse_args()
@@ -37,9 +36,6 @@ def main():
     elif args.chat:
         # src/bcs/main.py 실행 로직 추가
         subprocess.run(["python", "src/bcs/main.py"])
-    elif args.audit:
-        # Zeppelin 실행 및 audit/data 폴더 열기 로직 추가
-        print("Zeppelin 실행 및 audit/data 폴더 열기")
     elif args.ipconfig:
         # config/ip 파일 열기 로직 추가
         subprocess.run(["vim", "config/ip"])
