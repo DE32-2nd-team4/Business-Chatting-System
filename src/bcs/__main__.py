2d75b423f3c88988f019948a2a2b4448c25d18ea
tbongkim03 <tbongkim03@gmial.com>
2024-08-28 15:33:42 +0900

    daemon 옵션으로 bot 실행

--- src/bcs/__main__.py
+++ src/bcs/__main__.py
@@ -47,8 +47,16 @@ def main():
         # bot 실행
         processes = []
         for bot_file in ["src/bcs/system_bot.py", "src/bcs/aleam_bot.py", "src/bcs/movie_bot.py"]:
-            process = subprocess.Popen(["python", bot_file])
+            process = subprocess.Popen(
+                    ["python", bot_file],
+                    stdout=devnull,  # 표준 출력을 닫기
+                    stderr=devnull,  # 표준 오류 출력을 닫기
+                    stdin=devnull,   # 표준 입력을 닫기
+                    close_fds=True,  # 부모 프로세스의 파일 디스크립터를 닫기
+                    preexec_fn=os.setsid  # 새로운 세션을 생성하여 독립적으로 실행되도록 설정
+                    )
             processes.append(process)
+            print("bot 실행 완료")
 
         # 필요하다면, 모든 백그라운드 프로세스가 종료될 때까지 기다릴 수 있습니다.
         for process in processes:
